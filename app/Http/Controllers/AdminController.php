d9d18b79e88bb36916f81e340af5896b0cc6458e
Kartik B <kbiotics@gmail.com>
2024-12-24 19:35:38 +0530

    admin panel code completed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,31 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
 use App\Models\Post;
+use App\Models\User;
+use DB;
 
 class AdminController extends Controller
 {
 
-    public function Dashboard(){
+    public function Dashboard()
+    {
 
-        $totalUsers=User::count();
-        $totalPosts=Post::count();
+        $totalUsers = User::count();
+        $totalPosts = Post::count();
 
-        return view('pages/admin-panel/dashboard', compact('totalUsers','totalPosts'));
+        return view('pages/admin-panel/dashboard', compact('totalUsers', 'totalPosts'));
     }
 
-    public function UsersPage(){
+    public function UsersPage()
+    {
 
-        $users=User::get();
+        $users = User::get();
+
+        //alternate way via stored procedures
+        $users = DB::select('CALL test()', []);
 
         return view('pages/admin-panel/users', compact('users'));
     }
 
-    public function PostsPage(){
+    public function PostsPage()
+    {
 
-        $posts=Post::get();
+        $posts = Post::get();
+
+        //alternate way via stored procedures
+        $posts = DB::select('CALL posts_data()', []);
 
         return view('pages/admin-panel/posts', compact('posts'));
     }
--- resources/views/pages/admin-panel/users.blade.php
+++ resources/views/pages/admin-panel/users.blade.php
@@ -3,23 +3,43 @@
 @section('content')
 <h1>Users</h1>
 
-<table border=1>
-    <tr>
-    <th>id</th>
-    <th>name</th>
-    <th>email</th>
-    </tr>
 
-    @foreach($users as $user)
-    <tr>
-        <td><h3>{{$user->id}}</h3></td>
-        <td><img src="{{$user->profile_pic_path}}" alt="" width=50 height=50></td>
-        <td><h3>{{$user->name}}</h3></td>
-        <td><h3>{{$user->email}}</h3></td>
-    </tr>
-    @endforeach
 
-</table>
+
+                <!-- Recent Users -->
+                <div class="container-fluid pt-4 px-4">
+                <div class="bg-secondary text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Recent Users</h6>
+                        <a href="">Show All</a>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0">
+                            <thead>
+                                <tr class="text-white">
+                                    <th scope="col">id</th>
+                                    <th scope="col">profile picture</th>
+                                    <th scope="col">name</th>
+                                    <th scope="col">email</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td><h3>{{$user->id}}</h3></td>
+                                    <td><img src="{{$user->profile_pic_path}}" alt="" width=50 height=50></td>
+                                    <td><h3>{{$user->name}}</h3></td>
+                                    <td><h3>{{$user->email}}</h3></td>
+                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Recent Sales End -->
 
 
 
